305e301d07c356b6775942816b1d6046bc19d4f3
danger2123 <sellmysolenyc@gmail.com>
2021-08-19 10:02:51 -0400

    wrote partial getProducts function

---
+++ script.js
@@ -0,0 +1,30 @@
+let websiteUrl = document.getElementById("website-url");
+const getProductsBtn = document.getElementById("get-products-btn");
+let output = document.getElementById("output");
+
+getProductsBtn.addEventListener("click", getProducts);
+
+function getProducts() {
+  fetch(websiteUrl.value + "products.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let shopItems = data.products;
+      console.log(shopItems[0]);
+      let result = `<h1>Products</h1>`;
+      shopItems.forEach((product) => {
+        result += `<ul id="product-list">
+          <li><img src =${product.images[0].src} height="70" width="70"></li> 
+          <li>ID: ${product.id}</li>
+          <li>Name: ${product.title}</li>
+
+        </ul>`;
+      });
+      output.innerHTML = result;
+      // const productList = document.getElementById("product-list");
+      // for (let i = 0; i < shopItems.length; i++) {
+      //   productList.innerHTML += `<li>${shopItems.variants[i]}</li>`;
+      // }
+    });
+}
+
+// <li>Name: ${product.variants[0].id}</li>;
