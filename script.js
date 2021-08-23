afc928b92d90874ffed7c3353b95552b0e37866f
danger2123 <sellmysolenyc@gmail.com>
2021-08-23 07:56:53 -0400

    test

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function getProducts() {
   fetch(websiteUrl.value + "products.json")
     .then((res) => res.json())
     .then((data) => {
-      websiteUrl.value = "";
       let shopItems = data.products;
       console.log(shopItems[0]);
       let result = `<h1>Products</h1>`;
@@ -16,11 +15,21 @@ function getProducts() {
         result += `<ul id="product-list">
           <li><img src =${product.images[0].src} height="70" width="70"></li> 
           <li>ID: ${product.id}</li>
-          <li>Name: ${product.title}</li>
-
-        </ul>`;
+          <li>Name: ${product.title}</li>  
+        </ul>
+        <h2>Sizes</h2>
+        `;
+        for (let i = 0; i < shopItems.length; i++) {
+          for (let l = 0; l < product.variants.length; l++) {
+            result += `
+            <li><a href="${websiteUrl.value}/cart/${product.variants[l].id}:1">${product.variants[l].title}</a> - ${product.variants[l].id}</li>
+            `;
+          }
+        }
       });
       output.innerHTML = result;
+      websiteUrl.value = "";
+
       // const productList = document.getElementById("product-list");
       // for (let i = 0; i < shopItems.length; i++) {
       //   productList.innerHTML += `<li>${shopItems.variants[i]}</li>`;
